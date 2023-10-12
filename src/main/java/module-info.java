8e13161d705e6840b44bbba7b73b81e34078e5e1
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2023-10-12 20:59:55 +0600

    Working "Hello World" JavaFX App

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module me.shaphil.fxdemo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens me.shaphil.fxdemo to javafx.fxml;
+    exports me.shaphil.fxdemo;
+}
